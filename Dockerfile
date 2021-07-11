d32578a3fb67661c8ef6ff26de9c64787ca0ae7d
Egor Matveev <emmtvv@icloud.com>
2021-07-11 11:56:36 +0300

    postgres added

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.8
+
+ENV PYTHONUNBUFFERED 1
+ENV DJANGO_SETTINGS_MODULE Sprint.settings
+RUN mkdir -p /usr/src/app/
+WORKDIR /usr/src/app/
+
+COPY . /usr/src/app/
+
+RUN pip install -r requirements.txt
+
+EXPOSE 8000
--- Sprint/settings.py
+++ Sprint/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'Sprint.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': '0.0.0.0',
+        'PORT': '5432',
     }
 }
 
@@ -138,7 +142,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'data')
 
