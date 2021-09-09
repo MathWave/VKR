a45d3e104bd8b028ba4143623be88f14b6b992a5
Egor Matveev <emmtvv@icloud.com>
2021-09-09 09:38:18 +0300

    http

--- daemons/web.py
+++ daemons/web.py
@@ -3,4 +3,4 @@ from SprintLib.BaseDaemon import BaseDaemon
 
 class Daemon(BaseDaemon):
     def command(self):
-        return "python manage.py runserver 0.0.0.0:443"
+        return "python manage.py runserver 0.0.0.0:80"
