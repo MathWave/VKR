388a6964052e5c309064243759251e256d611277
Egor Matveev <emmtvv@icloud.com>
2021-09-09 09:34:29 +0300

    https

--- Sprint/settings.py
+++ Sprint/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "-w#*mn6*fa8a=(-c0@klx&$vl%hpiy&l(u*3%0a#2)wdt##(z2"
 
-DEPLOY = False
+DEPLOY = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = not DEPLOY
