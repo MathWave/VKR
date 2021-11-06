36d49313124957b88d36dbd74d38f3bf5a962b60
Egor Matveev <emmtvv@icloud.com>
2021-11-06 12:43:21 +0300

    bot to django command

--- Main/management/commands/bot.py
+++ Main/management/commands/bot.py
@@ -1,5 +1,5 @@
 import telebot
-
+from django.core.management.base import BaseCommand
 
 bot = telebot.TeleBot("1994460106:AAGrGsCZjF6DVG_T-zycELuVfxnWw8x7UyU")
 
@@ -10,7 +10,10 @@ def do_action(message):
     bot.send_message(chat_id=message.chat.id, text=f"ID чата: {message.chat.id}")
 
 
-if __name__ == '__main__':
-    print('bot is starting')
-    bot.polling()
-    print('bot failed')
+class Command(BaseCommand):
+    help = 'starts bot'
+
+    def handle(self, *args, **options):
+        print('bot is starting')
+        bot.polling()
+        print('bot failed')
