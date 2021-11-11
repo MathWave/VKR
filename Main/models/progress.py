cfc3aeae43bf70ef9717fbaa5fd01856ec22f0db
Egor Matveev <emmtvv@icloud.com>
2021-11-12 01:30:39 +0300

    progress

--- Main/models/progress.py
+++ Main/models/progress.py
@@ -19,7 +19,12 @@ class Progress(models.Model):
     def time(self):
         if not self.finished:
             self.finished_time = timezone.now()
-        return self.finished_time - self.start_time
+        seconds = (self.finished_time - self.start_time).total_seconds()
+        hours = seconds // 3600
+        seconds -= hours * 3600
+        minutes = seconds // 60
+        seconds -= minutes * 60
+        return f"{hours}:{minutes}:{seconds}"
 
     def increment_rating(self):
         if self.task.creator == self.user:
