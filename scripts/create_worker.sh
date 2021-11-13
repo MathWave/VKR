cfa89d3f57b00af1fa0706a32860f467b635ca50
Egor Matveev <emmtvv@icloud.com>
2021-11-14 01:24:27 +0300

    two workers

--- scripts/create_worker.sh
+++
@@ -1,6 +0,0 @@
-apk add --update --no-cache python3 && ln -sf python3 /usr/bin/python
-python3 -m ensurepip
-apk update && apk add postgresql-dev gcc python3-dev musl-dev jpeg-dev zlib-dev libjpeg
-pip3 install --no-cache --upgrade pip setuptools
-pip3 install -r requirements.txt
-python3 manage.py receive
