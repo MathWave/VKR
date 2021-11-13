8b772f25bbbb587db62679cd3ab2f70d48bf2456
Egor Matveev <emmtvv@icloud.com>
2021-11-13 18:05:37 +0300

    http

--- SprintLib/testers/BaseTester.py
+++ SprintLib/testers/BaseTester.py
@@ -56,7 +56,9 @@ class BaseTester:
         self.solution = solution
 
     def execute(self):
-        mkdir(str(self.solution.id))
+        if not exists("solutions"):
+            mkdir("solutions")
+        mkdir("solutions/" + str(self.solution.id))
         for file in SolutionFile.objects.filter(solution=self.solution):
             dirs = file.path.split('/')
             for i in range(len(dirs) - 1):
@@ -67,7 +69,7 @@ class BaseTester:
                 fs.write(get_bytes(file.fs_id))
         self.solution.result = CONSTS["testing_status"]
         self.solution.save()
-        docker_command = f"docker run --name solution_{self.solution.id} --volume=/sprint-data/worker/{self.solution.id}:/{self.working_directory} -t -d {self.solution.language.image}"
+        docker_command = f"docker run --name solution_{self.solution.id} --volume=/sprint-data/solutions/{self.solution.id}:/{self.working_directory} -t -d {self.solution.language.image}"
         print(docker_command)
         call(docker_command, shell=True)
         print("Container created")
