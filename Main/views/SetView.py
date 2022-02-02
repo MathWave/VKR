3bde9c11b9222127177f5a9de3a2893e1244e6e8
Egor Matveev <emmtvv@icloud.com>
2022-02-02 23:56:34 +0300

    set access

--- Main/views/SetView.py
+++ Main/views/SetView.py
@@ -1,7 +1,21 @@
-from SprintLib.BaseView import BaseView
+from django.utils import timezone
+
+from SprintLib.BaseView import BaseView, AccessError
 
 
 class SetView(BaseView):
     required_login = True
-    endpoint = 'set'
-    view_file = 'set.html'
+    endpoint = "set"
+    view_file = "set.html"
+
+    def get(self):
+        if self.entities.set in self.request.user.userinfo.available_sets:
+            return
+        if (
+            not self.entities.set.opened
+            or self.entities.set.start_time is not None
+            and self.entities.set.start_time > timezone.now()
+            or self.entities.set.end_time is not None
+            and self.entities.set.end_time < timezone.now()
+        ):
+            raise AccessError()
