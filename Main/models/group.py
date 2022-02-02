c22663d962b0456dc5041390ce1b2cfb198582de
Egor Matveev <emmtvv@icloud.com>
2022-02-03 00:10:17 +0300

    set access

--- Main/models/group.py
+++ Main/models/group.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
+from django.utils import timezone
+
 from Main.models.set import Set
 
 
@@ -10,3 +12,7 @@ class Group(models.Model):
     creator = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     users = models.ManyToManyField(User, related_name='user_groups')
     editors = ArrayField(models.TextField(), default=list)
+
+    @property
+    def available_sets(self):
+        return self.sets.filter(opened=True, start_time__lte=timezone.now(), end_time__gte=timezone.now())
