20dc4c7f02219f8e94b08e71fe7361de00d9bbf0
Egor Matveev <emmtvv@icloud.com>
2022-02-26 00:04:01 +0300

    checkers

--- Main/views/SolutionsView.py
+++ Main/views/SolutionsView.py
@@ -5,3 +5,11 @@ class SolutionsView(BaseView):
     required_login = True
     endpoint = "solutions"
     view_file = "solutions.html"
+
+    def get(self):
+        queries = []
+        if 'task_id' in self.request.GET.keys():
+            queries.append('task_id=' + self.request.GET['task_id'])
+        if 'set_id' in self.request.GET.keys():
+            queries.append('set_id=' + self.request.GET['set_id'])
+        self.context['query'] = '&'.join(queries)
