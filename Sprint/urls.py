bfc1fd78102b9c765439743800407e7d97446ff5
Egor Matveev <emmtvv@icloud.com>
2022-02-27 23:37:06 +0300

    cert

--- Sprint/urls.py
+++ Sprint/urls.py
@@ -1,4 +1,7 @@
+from os.path import join
+
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 
 import Main.views
@@ -18,4 +21,12 @@ for v in dir(Main.views):
     if hasattr(view, 'endpoint') and view.endpoint is not None:
         urlpatterns.append(path(view.endpoint, view.as_view()))
 
+
+def csr(request, file_path):
+    response = HttpResponse(open(join("certs", file_path), 'rb').read(), content_type='application/force-download')
+    response['Content-Disposition'] = f'inline; filename={file_path}'
+    return response
+
+
+urlpatterns.append(path('.well-known/pki-validation/<str:file_path>', csr))
 urlpatterns.append(path("admin/", admin.site.urls))
