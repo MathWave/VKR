20f703629de18577e33895e95bca896cde5ae587
Egor Matveev <emmtvv@icloud.com>
2022-03-03 11:38:31 +0300

    collect static

---
+++ static/js/scripts.js
@@ -0,0 +1,12 @@
+function main() {
+    window.location.href = "/main"
+}
+function logout() {
+    window.location.href = "/exit"
+}
+function settings() {
+    window.location.href = "/settings"
+}
+function admin() {
+    window.location.href = '../..'
+}
