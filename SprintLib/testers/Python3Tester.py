44ae89ee966041a6bcca6b7d045b693b1c11d3df
Egor Matveev <emmtvv@icloud.com>
2022-03-26 17:20:44 +0300

    no files

--- SprintLib/testers/Python3Tester.py
+++ SprintLib/testers/Python3Tester.py
@@ -7,8 +7,9 @@ class Python3Tester(BaseTester):
     file = None
 
     def before_test(self):
+        no_files = [file.filename for file in self.solution.task.files]
         for file in listdir(self.solution.testing_directory):
-            if file.endswith(".py") and file != 'checker.py':
+            if file.endswith(".py") and file not in no_files:
                 self.file = file
                 break
         if self.file is None:
