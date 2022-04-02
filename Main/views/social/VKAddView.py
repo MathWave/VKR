80bb8a3dbdc38d6c76fb6bb10e3dc658bf8d1c3c
Egor Matveev <emmtvv@icloud.com>
2022-04-02 19:07:18 +0300

    version

--- Main/views/social/VKAddView.py
+++ Main/views/social/VKAddView.py
@@ -21,7 +21,7 @@ class VKAddView(BaseView):
         token = os.getenv("VK_SERVICE_TOKEN")
         resp = get(f'https://api.vk.com/method/secure.checkToken?token={access_token}&access_token={token}&v=5.131').json()
         print("Got response while adding user", resp)
-        if 'success' in resp and resp['success'] == 1:
+        if 'response' in resp and 'success' in resp['response'] and resp['response']['success'] == 1:
             user_id = resp['user_id']
             self.request.user.userinfo.vk_user_id = user_id
             self.request.user.userinfo.save()
